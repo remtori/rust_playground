a6055ac393b9e713d0533cedce899180a122b041
remtori <lqvu99@gmail.com>
2021-02-17 22:17:13 +0700

    context: temp global variables

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 #![feature(default_free_fn)]
 
+use std::collections::HashMap;
+
 pub mod ast;
 pub mod js;
 pub mod parser;
@@ -8,10 +10,22 @@ pub mod parser;
 extern crate lazy_static;
 
 #[derive(Debug, Default)]
-pub struct Context;
+pub struct Context {
+    variables: HashMap<String, js::Value>,
+}
 
 impl Context {
     pub fn new() -> Context {
-        Context {}
+        Context {
+            variables: HashMap::new(),
+        }
+    }
+
+    pub fn get_variable(&self, name: &str) -> Option<js::Value> {
+        self.variables.get(name).map(Clone::clone)
+    }
+
+    pub fn set_variable(&mut self, name: String, value: js::Value) {
+        self.variables.insert(name, value);
     }
 }
