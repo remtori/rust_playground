c310367ef8bc453a94b6fba40137588e9e16f3ba
remtori <lqvu99@gmail.com>
2021-02-17 18:48:10 +0700

    value: Added all primitive values although some are unimplemented

--- src/js.rs
+++ src/js.rs
@@ -1,15 +1,16 @@
 use std::*;
-
 use crate::*;
 
-#[derive(Clone)]
 pub enum Value {
     Undefined,
     Null,
     Boolean(bool),
-    Number(f64),
+    Rational(f64),
+    Integer(i32),
+    BigInt(()),
     String(String),
     Object(()),
+    Symbol(()),
 }
 
 pub enum PreferredType {
@@ -20,7 +21,7 @@ pub enum PreferredType {
 
 impl ast::ASTNode for Value {
     fn eval(&mut self, _: &mut Context) -> Value {
-        self.clone()
+        *self
     }
 }
 
@@ -30,9 +31,12 @@ impl fmt::Display for Value {
             Value::Undefined => write!(f, "Value{{undefined}}"),
             Value::Null => write!(f, "Value{{null}}"),
             Value::Boolean(b) => write!(f, "Value{{bool: {}}}", b),
-            Value::Number(v) => write!(f, "Value{{number: {}}}", v),
+            Value::Rational(v) => write!(f, "Value{{number: {}}}", v),
+            Value::Integer(v) => write!(f, "Value{{number: {}}}", v),
             Value::String(s) => write!(f, "Value{{string: {}}}", s),
             Value::Object(_) => write!(f, "Value{{object}}"),
+            Value::BigInt(_) => write!(f, "Value{{bigint}}"),
+            Value::Symbol(_) => write!(f, "Value{{symbol}}"),
         }
     }
 }
@@ -44,12 +48,43 @@ impl fmt::Debug for Value {
 }
 
 impl Value {
+
+    pub fn is_null(&self) -> bool {
+        matches!(self, Value::Null)
+    }
+
+    pub fn is_undefined(&self) -> bool {
+        matches!(self, Value::Undefined)
+    }
+
+    pub fn is_bigint(&self) -> bool {
+        matches!(self, Value::BigInt(_))
+    }
+
+    pub fn is_nan(&self) -> bool {
+        if let Value::Rational(v) = self {
+            v.is_nan()
+        } else {
+            false
+        }
+    }
+
+    pub fn number_from_str(str: &str) -> Value {
+        if let Ok(v) = str.parse::<i32>() {
+            Value::Integer(v)
+        } else if let Ok(v) = str.parse::<f64>() {
+            Value::Rational(v)
+        } else {
+            Value::Rational(f64::NAN)
+        }
+    }
+
     /// JS Value type conversion
     pub fn to_primitive(&self, _preferred_type: PreferredType) -> Value {
         if let Value::Object(_obj) = self {
             todo!()
         } else {
-            self.clone()
+            *self
         }
     }
 
@@ -58,7 +93,9 @@ impl Value {
             Value::Undefined => false,
             Value::Null => false,
             Value::Boolean(v) => *v,
-            Value::Number(v) => !(v.is_nan() && *v == 0.0),
+            Value::Rational(v) => !(v.is_nan() && *v == 0.0),
+            Value::Integer(v) => *v == 0,
+            Value::BigInt(_) => todo!(),
             Value::String(s) => !s.is_empty(),
             Value::Object(_) => true,
         }
@@ -66,23 +103,50 @@ impl Value {
 
     pub fn to_number(&self) -> Value {
         match self {
-            Value::Undefined => Value::Number(f64::NAN),
-            Value::Null => Value::Number(0.0),
-            Value::Boolean(b) => Value::Number(if *b { 1.0 } else { 0.0 }),
-            Value::Number(v) => Value::Number(*v),
-            Value::String(s) => Value::Number(*s.parse::<f64>().ok().get_or_insert(f64::NAN)),
+            Value::Undefined => Value::Rational(f64::NAN),
+            Value::Null => Value::Integer(0),
+            Value::Boolean(b) => Value::Integer(if *b { 1 } else { 0 }),
+            Value::Rational(_) => *self,
+            Value::Integer(_) => *self,
+            Value::BigInt(_) => todo!("TypeError"),
+            Value::Symbol(_) => todo!("TypeError"),
+            Value::String(s) => Value::Rational(*s.parse::<f64>().ok().get_or_insert(f64::NAN)),
             Value::Object(_) => self.to_primitive(PreferredType::Number).to_number(),
         }
     }
 
-    pub fn spec_to_string(&self) -> String {
+    pub fn to_numeric(&self) -> Value {
+        let primitive_value = self.to_primitive(PreferredType::Number);
+        if primitive_value.is_bigint() {
+            primitive_value
+        } else {
+            primitive_value.to_number()
+        }
+    }
+
+    pub fn to_string(&self) -> String {
         match self {
             Value::Undefined => "undefined".to_owned(),
             Value::Null => "null".to_owned(),
             Value::Boolean(v) => v.to_string(),
-            Value::Number(v) => v.to_string(),
-            Value::String(v) => v.clone(),
-            Value::Object(_) => self.to_primitive(PreferredType::String).spec_to_string(),
+            Value::Rational(v) => v.to_string(),
+            Value::Integer(v) => v.to_string(),
+            Value::BigInt(_) => todo!(),
+            Value::String(v) => *v,
+            Value::Object(_) => self.to_primitive(PreferredType::String).to_string(),
+            Value::Symbol(_) => todo!("TypeError"),
         }
+    }
+}
+
+impl From<f64> for Value {
+    fn from(v: f64) -> Self {
+        Value::Rational(v)
+    }
+}
+
+impl From<i32> for Value {
+    fn from(v: i32) -> Self {
+        Value::Integer(v)
     }
 }
