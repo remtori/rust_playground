1082cab26462c57426491806af42373372cc08bb
remtori <lqvu99@gmail.com>
2021-03-04 09:34:11 +0700

    lib: utils: added logger via 3rd party crate

--- lib/utils/src/lib.rs
+++ lib/utils/src/lib.rs
@@ -1,3 +1,34 @@
+pub mod prelude;
 pub mod string;
 pub mod threadguard;
 pub mod threadpool;
+
+pub fn init_logger() -> Result<(), fern::InitError> {
+    use fern::colors::*;
+    use fern::*;
+    use std::*;
+
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::Magenta)
+        .trace(Color::White);
+
+    Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{}[{:5}][{}][{}]: {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                colors.color(record.level()),
+                record.target(),
+                thread::current().name().get_or_insert("unnamed"),
+                message
+            ))
+        })
+        .level(log::LevelFilter::Debug)
+        .chain(std::io::stdout())
+        .apply()?;
+
+    Ok(())
+}
---
+++ lib/utils/src/prelude.rs
@@ -0,0 +1,7 @@
+pub use super::string::*;
+pub use super::threadguard::*;
+pub use super::threadpool::*;
+
+// External crate
+pub use chrono;
+pub use log::{debug, error, info, trace, warn};
