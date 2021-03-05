833806dfd5b0523c72ff3dba5afcb27158b2fbdd
remtori <lqvu99@gmail.com>
2021-03-05 17:12:08 +0700

    lib: http: HttpRequest parsing

---
+++ lib/http/src/http.rs
@@ -0,0 +1,226 @@
+use std::*;
+use utils::prelude::*;
+
+pub const EMPTY_HEADER: Header<'static> = Header {
+    field: "",
+    value: b"",
+};
+
+#[allow(clippy::upper_case_acronyms)]
+#[derive(Debug)]
+pub enum Method {
+    GET,
+    POST,
+    HEAD,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+    Ext(String),
+}
+
+#[derive(Debug)]
+pub enum Version {
+    V10,
+    V11,
+    Unknown,
+}
+
+#[derive(Debug)]
+pub struct Header<'b> {
+    field: &'b str,
+    value: &'b [u8],
+}
+
+#[derive(Debug)]
+pub enum ParseError {
+    PartialData,
+    ParseUtf8Err,
+    InvalidUri,
+    InvalidVersion,
+    InvalidHeaderField,
+    InvalidHeaderValue,
+    TooManyHeader,
+}
+
+impl From<core::str::Utf8Error> for ParseError {
+    fn from(_: core::str::Utf8Error) -> Self {
+        Self::ParseUtf8Err
+    }
+}
+
+type Result<T> = core::result::Result<T, ParseError>;
+
+#[derive(Debug)]
+pub struct HttpRequest<'buf, 'h> {
+    method: Method,
+    uri: &'buf str,
+    version: Version,
+    headers: &'h mut [Header<'buf>],
+    header_count: usize,
+}
+
+impl<'buf, 'h> HttpRequest<'buf, 'h> {
+    pub fn new(headers: &'h mut [Header<'buf>]) -> HttpRequest<'buf, 'h> {
+        HttpRequest {
+            method: Method::GET,
+            uri: "",
+            version: Version::Unknown,
+            headers,
+            header_count: 0,
+        }
+    }
+
+    pub fn parse(&mut self, buffer: &'buf [u8]) -> Result<usize> {
+        let mut bytes = Bytes::from(buffer);
+
+        self.method = parse_method(&mut bytes)?;
+
+        // Nom whitespace
+        if !bytes.consume(b' ').ok_or(ParseError::PartialData)? {
+            return Err(ParseError::InvalidUri);
+        }
+
+        let uri = bytes.consume_until(b' ').ok_or(ParseError::PartialData)?;
+        self.uri = if let Ok(v) = str::from_utf8(uri) {
+            v
+        } else {
+            return Err(ParseError::ParseUtf8Err);
+        };
+
+        // Nom whitespace
+        if !bytes.consume(b' ').ok_or(ParseError::PartialData)? {
+            return Err(ParseError::InvalidUri);
+        }
+
+        self.version = parse_version(&mut bytes, true)?;
+
+        // Nom \r\n
+        if let Some(ok) = bytes.consume_str(b"\r\n") {
+            if !ok {
+                return Err(ParseError::InvalidHeaderField);
+            }
+        } else {
+            return Err(ParseError::PartialData);
+        }
+
+        self.header_count = parse_headers(&mut bytes, &mut self.headers)?;
+
+        Ok(bytes.offset())
+    }
+}
+
+fn parse_method(buffer: &mut Bytes) -> Result<Method> {
+    match buffer.consume_until(b' ') {
+        Some(b"GET") => Ok(Method::GET),
+        Some(b"POST") => Ok(Method::POST),
+        Some(b"HEAD") => Ok(Method::HEAD),
+        Some(b"PUT") => Ok(Method::PUT),
+        Some(b"DELETE") => Ok(Method::DELETE),
+        Some(b"CONNECT") => Ok(Method::CONNECT),
+        Some(b"OPTIONS") => Ok(Method::OPTIONS),
+        Some(b"TRACE") => Ok(Method::TRACE),
+        Some(b"PATCH") => Ok(Method::PATCH),
+        Some(s) => Ok(Method::Ext(str::from_utf8(s)?.to_string())),
+        None => Err(ParseError::PartialData),
+    }
+}
+
+fn parse_version(buffer: &mut Bytes, is_request: bool) -> Result<Version> {
+    if let Some(ok) = buffer.consume_str(b"HTTP/") {
+        if !ok {
+            return Err(ParseError::InvalidVersion);
+        }
+    } else {
+        return Err(ParseError::PartialData);
+    }
+
+    let s = if is_request {
+        buffer.consume_until(b'\r')
+    } else {
+        buffer.consume_until(b' ')
+    };
+
+    match s {
+        Some(b"1.0") => Ok(Version::V10),
+        Some(b"1.1") => Ok(Version::V11),
+        Some(_) => Err(ParseError::InvalidVersion),
+        None => Err(ParseError::PartialData),
+    }
+}
+
+fn parse_headers<'a, 'buf>(
+    buffer: &'a mut Bytes<'buf>,
+    headers: &mut &mut [Header<'buf>],
+) -> Result<usize> {
+    let mut header_count = 0usize;
+    let mut iter = headers.iter_mut();
+
+    loop {
+        if let Some(ok) = buffer.consume_str(b"\r\n") {
+            if ok {
+                break;
+            }
+        } else {
+            return Err(ParseError::PartialData);
+        }
+
+        let field = buffer.consume_until(b':');
+        if field.is_none() {
+            return Err(ParseError::PartialData);
+        }
+
+        if let Some(ok) = buffer.consume_str(b": ") {
+            if !ok {
+                return Err(ParseError::InvalidHeaderField);
+            }
+        } else {
+            return Err(ParseError::PartialData);
+        }
+
+        let field = field.unwrap();
+        let field = if let Ok(v) = str::from_utf8(field) {
+            v
+        } else {
+            return Err(ParseError::ParseUtf8Err);
+        };
+
+        let value = buffer.consume_until(b'\r');
+        if value.is_none() {
+            return Err(ParseError::PartialData);
+        }
+
+        if let Some(ok) = buffer.consume_str(b"\r\n") {
+            if !ok {
+                return Err(ParseError::InvalidHeaderValue);
+            }
+        } else {
+            return Err(ParseError::PartialData);
+        }
+
+        let header = match iter.next() {
+            Some(header) => header,
+            None => return Err(ParseError::TooManyHeader),
+        };
+
+        header.field = field;
+        header.value = value.unwrap();
+        header_count += 1;
+    }
+
+    Ok(header_count)
+}
+
+#[derive(Debug)]
+pub struct HttpResponse<'buf, 'h> {
+    version: Version,
+    status: u16,
+    status_description: &'buf str,
+    headers: &'h [Header<'buf>],
+}
+
+impl<'buf, 'h> HttpResponse<'buf, 'h> {
+    // fn parse(buffer: &'buf [u8], headers: &'h [Header<'buf>]) -> Result<HttpResponse<'buf, 'h>> {}
+}
