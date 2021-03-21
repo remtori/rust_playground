17c854bfd866206a6e875d1c98abf19a5ebb1ee1
remtori <lqvu99@gmail.com>
2021-03-21 22:51:55 +0700

    lib: vm: a function created at runtime that return 1

---
+++ lib/vm/src/main.rs
@@ -0,0 +1,25 @@
+#![allow(clippy::upper_case_acronyms)]
+
+use memmap2::MmapMut;
+use std::io::Write;
+
+use std::*;
+
+const PROGRAM: &[u8] = &[0xb8, 0x01, 0x00, 0x00, 0x00, 0xc3];
+
+fn main() -> Result<(), std::io::Error> {
+    let mut mmap = MmapMut::map_anon(32)?;
+
+    (&mut mmap[..]).write_all(PROGRAM)?;
+    println!("Write");
+
+    let exec_map = mmap.make_exec()?;
+    println!("Make exec");
+
+    let func: extern "C" fn() -> bool = unsafe { mem::transmute(exec_map.as_ptr()) };
+    println!("Cast");
+
+    println!("ret={}", func());
+
+    Ok(())
+}
