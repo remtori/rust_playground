e00c1d2bd3f775ec2c24d540b491102ef4f98831
remtori <lqvu99@gmail.com>
2021-03-25 21:55:17 +0700

    client: run prettier

--- client/src/main.ts
+++ client/src/main.ts
@@ -7,32 +7,30 @@ context.fillStyle = '#fff';
 context.fillRect(0, 0, canvas.width, canvas.height);
 
 let isPressed = false;
-canvas.addEventListener('mousedown', e => {
-    if (e.button === 0)
-        isPressed = true;
+canvas.addEventListener('mousedown', (e) => {
+	if (e.button === 0) isPressed = true;
 });
 
-canvas.addEventListener('mouseup', e => {
-    if (e.button == 0)
-        isPressed = false;
+canvas.addEventListener('mouseup', (e) => {
+	if (e.button == 0) isPressed = false;
 });
 
-canvas.addEventListener('mouseleave', e => {
-    isPressed = false;
+canvas.addEventListener('mouseleave', (e) => {
+	isPressed = false;
 });
 
-canvas.addEventListener('mousemove', e => {
-    if (isPressed) {
-        context.fillStyle = '#000';
-        context.fillRect(e.offsetX, e.offsetY, 2, 2);
-        remote.send(new Uint32Array([ e.offsetX, e.offsetY ]));
-    }
+canvas.addEventListener('mousemove', (e) => {
+	if (isPressed) {
+		context.fillStyle = '#000';
+		context.fillRect(e.offsetX, e.offsetY, 2, 2);
+		remote.send(new Uint32Array([e.offsetX, e.offsetY]));
+	}
 });
 
-remote.onmessage = e => {
-    e.data.arrayBuffer().then((buffer: ArrayBuffer) => {
-        const view = new Uint32Array(buffer);
-        context.fillStyle = '#f00';
-        context.fillRect(view[0], view[1], 2, 2);
-    })
-}
+remote.onmessage = (e) => {
+	e.data.arrayBuffer().then((buffer: ArrayBuffer) => {
+		const view = new Uint32Array(buffer);
+		context.fillStyle = '#f00';
+		context.fillRect(view[0], view[1], 2, 2);
+	});
+};
