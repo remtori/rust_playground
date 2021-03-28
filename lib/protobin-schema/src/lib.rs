b1e549e4d3eaee18c29f0ce2c0de5c252ba44700
remtori <lqvu99@gmail.com>
2021-03-28 12:20:00 +0700

    lib: protobin-schema: remove custom parser use syn + proc_macro2

---
+++ lib/protobin-schema/src/lib.rs
@@ -0,0 +1,80 @@
+use proc_macro2::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{parse2, parse_quote, Data, DeriveInput, Error, Fields, GenericParam, Generics};
+
+pub fn derive_message(input: TokenStream) -> TokenStream {
+    let input = match parse2::<DeriveInput>(input) {
+        Ok(data) => data,
+        Err(err) => return err.to_compile_error(),
+    };
+
+    let name = input.ident;
+    let generics = add_trait_bounds(input.generics);
+    let (impl_generics, ty_generics, where_clause) = generics.split_for_impl();
+
+    let serialize_body = match serialize_impl(&input.data) {
+        Ok(data) => data,
+        Err(err) => return Error::new(name.span(), err).to_compile_error(),
+    };
+
+    quote! {
+        impl #impl_generics protobin::Serialize for #name #ty_generics #where_clause {
+            fn wire_type(&self) -> protobin::WireType {
+                protobin::WireType::VarLen
+            }
+
+            fn serialize(&self, serializer: &mut Serializer) -> Result<()> {
+                #serialize_body
+                Ok(())
+            }
+        }
+
+        impl #impl_generics protobin::Deserialize for #name #ty_generics #where_clause {
+            fn deserialize(deserializer: &mut Deserializer) -> Result<Self> {
+                todo!()
+            }
+        }
+    }
+}
+
+// Add a bound `T: Serialize + Deserialize` to every type parameter T.
+fn add_trait_bounds(mut generics: Generics) -> Generics {
+    for param in &mut generics.params {
+        if let GenericParam::Type(type_param) = param {
+            type_param
+                .bounds
+                .push(parse_quote!(protobin::Serialize + protobin::Deserialize));
+        }
+    }
+    generics
+}
+
+fn serialize_impl(data: &Data) -> Result<TokenStream, &'static str> {
+    match data {
+        Data::Struct(struct_data) => match &struct_data.fields {
+            Fields::Named(fields) => {
+                let len = fields.named.len() as u32;
+
+                let recurse = fields.named.iter().enumerate().map(|(idx, field)| {
+                    let idx = (idx + 1) as u32;
+                    let name = &field.ident;
+
+                    quote_spanned! { field.span() =>
+                        serializer.write_field(#idx, &self.#name)?;
+                    }
+                });
+
+                Ok(quote! {
+                    serializer.write_u32(#len)?;
+                    #(#recurse)*
+                })
+            }
+            Fields::Unnamed(_) | Fields::Unit => Err("expected struct with named field"),
+        },
+        Data::Enum(_) => {
+            todo!()
+        }
+        Data::Union(_) => Err("expected struct or enum"),
+    }
+}
--- lib/protobin-schema/src/main.rs
+++ lib/protobin-schema/src/main.rs
@@ -1,45 +1,13 @@
-use crate::{
-    context::{Context, Language},
-    error::Result,
-    parser::Parser,
-};
-
-mod context;
-mod error;
-mod lexer;
-mod parser;
-mod token;
-
-fn exec() -> Result<()> {
-    let source = r#"
-        /// Send ping message to check if the other party is alive
-        struct Ping {
-            /// Sender timestamp
-            unixTime: u32,
-        }
-
-        // comment break stuff
-        struct Pong : Ping {}
-
-        struct A: B{}
-    "#;
-
-    let mut context = Context::new();
-
-    let mut parser = Parser::new(source);
-    parser.parse_schema(&mut context)?;
-
-    println!("{:#?}", context);
-
-    context.validate()?;
-
-    context.export(Language::Rust, &mut std::io::stdout())?;
-
-    Ok(())
-}
+use std::str::FromStr;
 
 fn main() {
-    if let Err(err) = exec() {
-        println!("{}", err);
-    }
+    let source = r#"
+        struct Ping {
+            unix_time: u32,
+        }
+    "#;
+
+    let tokens = proc_macro2::TokenStream::from_str(&source).unwrap();
+    let output = protobin_schema::derive_message(tokens);
+    println!("{}", output);
 }
